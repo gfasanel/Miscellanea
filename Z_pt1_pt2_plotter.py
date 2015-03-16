aadc6e8a09898ea12f0d7ea0d2055594cc95310f
gfasanel <giuseppe.fasanella@cern.ch>
2015-03-16 17:57:58 +0100

    fixing a bug in reco matching

--- Z_pt1_pt2_plotter.py
+++ Z_pt1_pt2_plotter.py
@@ -118,6 +118,7 @@ for det in detector_regions:
    hist['pt1_Over_pt2_gen'][det]={}
 
 histo_ptZ_gen=ROOT.TH1F("ptZ_gen","ptZ_gen",100,0,100)
+histo_massZ_gen=ROOT.TH1F("massZ_gen","massZ_gen",150,0,150)
 #histo_ptZ_reco=ROOT.TH1F("ptZ_reco","ptZ_reco",100,0,100)
 for region in pt_regions:
    print regions[region]['name']
@@ -131,12 +132,11 @@ for region in pt_regions:
       hist['pt2_gen'][det][regions[region]['name']]=ROOT.TH1F(str('pt2_gen_'+det+'_'+regions[region]['name']),str('pt2_gen_'+det+'_'+regions[region]['name']),100,0,100)
       hist['pt1_Over_pt2_gen'][det][regions[region]['name']]=ROOT.TH1F(str('pt1_Over_pt2_gen_'+det+'_'+regions[region]['name']),str('pt1_Over_pt2_gen_'+det+'_'+regions[region]['name']),100,0,3)
 
-canvas={}# a comparison plot for each region
-for region in pt_regions:
-   canvas[str(region)]=ROOT.TCanvas(str(region),str(region))
-
 # loop over the events
+counter = 0
 for iev,event in enumerate(events):
+
+    counter =counter +1
     #if iev > 100: break #For quick tests
     #print iev #Ti stampa il numero dell'evento che stai considerando
     # use getByLabel, just like in cmsRun
@@ -160,71 +160,72 @@ for iev,event in enumerate(events):
 
     detector_descriptor='none'
 
-    for igen, genParticle in enumerate(gen_particles):#loopo sulle particelle generate
-
-        if (abs(genParticle.pdgId())==11 and genParticle.mother().pdgId()==23 ):# se si tratta di un elettrone di madre Z
+    for igen, genParticle in enumerate(gen_particles): #loop over generated particles
+        if (abs(genParticle.pdgId())==11 and genParticle.mother().pdgId()==23 ):# it must be an electron, daughter of a Z boson
             if(gen0==0):
+                #print "Electron 0: ID, Mother ID", genParticle.pdgId(),genParticle.mother().pdgId()
                 gen_electron0= gen_electron(genParticle.px(),genParticle.py(),genParticle.pz(),genParticle.energy())
-                vector_gen = ROOT.TLorentzVector(genParticle.px(),genParticle.py(),genParticle.pz(),genParticle.energy())
+                #print 'gen px,py,pz,E',gen_electron0.p4.Px(),gen_electron0.p4.Py(),gen_electron0.p4.Pz(),gen_electron0.p4.E()
                 gen0=1
                 pt1_gen=genParticle.pt()
 
                 for iele, electron in enumerate(electrons): #loop over reconstructed
                     vector_reco = ROOT.TLorentzVector(electron.px(),electron.py(),electron.pz(),electron.energy())
                     dr=999
-                    dr=vector_reco.DeltaR(vector_gen)
-                    if dr<0.15: # matching avvenuto
+                    #dr=vector_reco.DeltaR(vector_gen)
+                    dr=vector_reco.DeltaR(gen_electron0.p4)
+                    if dr<0.15:
                         ismatched0=1 #This tells if the gen0 is reconstructed
                         pt1=vector_reco.Pt()
                     
             elif (gen0==1 and gen1==0):
+                #print "Electron 1: ID, Mother ID", genParticle.pdgId(),genParticle.mother().pdgId()
                 gen_electron1= gen_electron(genParticle.px(),genParticle.py(),genParticle.pz(),genParticle.energy())
-                vector_gen1 = ROOT.TLorentzVector(genParticle.px(),genParticle.py(),genParticle.pz(),genParticle.energy())
                 gen1=1
                 pt2_gen=genParticle.pt()
 
                 Z=Zboson_object(gen_electron0,gen_electron1)
                 vector_Z_gen.SetPxPyPzE(genParticle.mother().px(),genParticle.mother().py(),genParticle.mother().pz(),genParticle.mother().energy())
                 detector_descriptor=Z.regions
-                #if detector_descriptor=='BE':
-                   #print 'la regione BE'
-
-                   #print 'my Z object Pt ',Z.p4.Pt()
-                   #print 'truth Z Pt ',vector_Z_gen.Pt()
-                #print 'region ', Z.regions
 
                 for iele, electron in enumerate(electrons): #loop over reconstructed
                     vector_reco1 = ROOT.TLorentzVector(electron.px(),electron.py(),electron.pz(),electron.energy())
                     dr=999
-                    dr=vector_reco1.DeltaR(vector_gen1)
-                    if dr<0.15: # matching avvenuto
-                        ismatched1=1
+                    dr=vector_reco1.DeltaR(gen_electron1.p4)
+                    if dr<0.15:
+                        ismatched1=1 #This tells if the gen1 is reconstructed
                         pt2=vector_reco1.Pt()
-                    break #hai trovato due elettroni e visto se vengono ricostruiti: puoi chiudere il ciclo sulle generate
-    
-    #pt_mother = genParticle.mother().pt()
-    #print 'pt1,pt2',pt1,pt2
-    #print 'pt1_gen,pt2_gen',pt1_gen,pt2_gen
+                    #break #hai trovato due elettroni e visto se vengono ricostruiti: puoi chiudere il ciclo sulle generate
+
     if (gen0 and gen1):
         for region in pt_regions: 
-            if ((vector_Z_gen.Pt()) >= regions[region]['ptmin'] and (vector_Z_gen.Pt()) < regions[region]['ptmax'] and detector_descriptor!='none'):
-                #print 'I am filling'
-                #print 'Pt mother from ROOT ', vector_Z_gen.Pt()
-                #print 'Pt mother from the file ', pt_mother
-                #if(detector_descriptor=='BE'):
-                   #print pt1_gen/pt2_gen
+            if ( vector_Z_gen.Pt() >= regions[region]['ptmin'] and vector_Z_gen.Pt() < regions[region]['ptmax'] and detector_descriptor!='none' and vector_Z_gen.M() >= 70 and vector_Z_gen.M() <=110 ):
+                if(pt1_gen < pt2_gen): #swap if 2 is the leading
+                   temp=pt1_gen
+                   pt1_gen=pt2_gen
+                   pt2_gen=temp
+
                 hist['pt1_gen'][detector_descriptor][regions[region]['name']].Fill(pt1_gen)
                 hist['pt2_gen'][detector_descriptor][regions[region]['name']].Fill(pt2_gen)
                 hist['pt1_Over_pt2_gen'][detector_descriptor][regions[region]['name']].Fill(pt1_gen/pt2_gen)
                 histo_ptZ_gen.Fill(vector_Z_gen.Pt())
+                histo_massZ_gen.Fill(vector_Z_gen.M())
+
     if (ismatched0 and ismatched1):
         for region in pt_regions: 
-            if ((vector_Z_gen.Pt()) >= regions[region]['ptmin'] and (vector_Z_gen.Pt()) < regions[region]['ptmax'] and detector_descriptor!='none'):
+            if (vector_Z_gen.Pt() >= regions[region]['ptmin'] and vector_Z_gen.Pt() < regions[region]['ptmax'] and detector_descriptor!='none' and vector_Z_gen.M() >= 70 and vector_Z_gen.M() <=110):
+                if(pt1 < pt2): #swap if 2 is the leading
+                   temp=pt1
+                   pt1=pt2
+                   pt2=temp
                 hist['pt1_reco'][detector_descriptor][regions[region]['name']].Fill(pt1)
                 hist['pt2_reco'][detector_descriptor][regions[region]['name']].Fill(pt2)
                 hist['pt1_Over_pt2_reco'][detector_descriptor][regions[region]['name']].Fill(pt1/pt2)
 
 # draw everything and save the histos
+
+print "Total number of events",counter
+
 
 #If directory doesn't exist, then create it
 if not os.path.exists('~/scratch1/www/Pt1Pt2/pt1_pt2_plots'):
@@ -244,3 +245,4 @@ for region in pt_regions:
        hist['pt1_Over_pt2_reco'][det][regions[region]['name']].Write()
 
 histo_ptZ_gen.Write()
+histo_massZ_gen.Write()
